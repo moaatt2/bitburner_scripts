eeecaa8655d4a17629b2b2b8edb1be8c7322a557
Dakota McKay <dakotamckay72@gmail.com>
2024-01-20 08:59:21 -0500

    A more advanced server pwning script that searches for an automatically pwns all possible servers.

---
+++ utility_scripts/pwn_02/auto_pwn.js
@@ -0,0 +1,53 @@
+/** @param {NS} ns */
+export async function main(ns) {
+
+    // Set Constants
+    const host = ns.getHostname();
+
+    // Create array containing all server names
+    let servers = [host];
+    let i = 0;
+    while (i < servers.length) {
+        let scan_server = servers[i];
+        let scanned_servers = ns.scan(scan_server)
+
+        let j=0;
+        while (j < scanned_servers.length) {
+            let server = scanned_servers[j];
+            if (!servers.includes(server)) {
+                servers.push(server)
+            }
+            j++;
+        }
+        i++;
+    }
+
+
+    // Get Current Player Hacking Level
+    let ph = ns.getHackingLevel();
+
+    // Determine how many port opening functions I have access to
+    let port_opens = 0
+    if (ns.fileExists("BruteSSH.exe", host)) {port_opens++;}
+    if (ns.fileExists("FTPCrack.exe", host)) {port_opens++;}
+    if (ns.fileExists("relaySMTP.exe", host)) {port_opens++;}
+    if (ns.fileExists("HTTPWorm.exe", host)) {port_opens++;}
+    if (ns.fileExists("SQLInject.exe", host)) {port_opens++;}
+
+    // Itterate over all servers without backdoor access
+    for (let server of servers) {
+        if (!ns.hasRootAccess(server)) {
+            let sh = ns.getServerRequiredHackingLevel(server);
+            let sp = ns.getServerNumPortsRequired(host);
+
+            let can_pwn = (sh < ph) & (sp <= port_opens);
+
+            if (can_pwn) {
+                ns.run('pwn.js', 1, server);
+                ns.tprint('Pwnd '+server)
+                await ns.sleep(1000);
+            }
+        }
+    }
+
+}
